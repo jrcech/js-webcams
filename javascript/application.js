edc91325ae915c7bb2b275500209b200a82b7d55
Jiří Čech <jiricech94@gmail.com>
2021-01-06 23:13:19 +0100

    Add country select2

--- javascript/application.js
+++ javascript/application.js
@@ -4,6 +4,7 @@ import Turbo from "@hotwired/turbo";
 
 import "./stylesheets";
 import "jquery";
+import "select2";
 
 const application = Application.start();
 const context = require.context("./controllers", true, /\.js$/);
