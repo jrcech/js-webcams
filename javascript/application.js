9cb72a3af13cffa4b6b794bd36dc22fc659e02a4
Jiří Čech <jiricech94@gmail.com>
2021-01-06 16:11:37 +0100

    Configure webpack

---
+++ javascript/application.js
@@ -0,0 +1,10 @@
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
+import Turbo from "@hotwired/turbo";
+
+import "./stylesheets";
+
+const application = Application.start();
+const context = require.context("./controllers", true, /\.js$/);
+
+application.load(definitionsFromContext(context));
---
+++ javascript/config/config.base.js
@@ -0,0 +1,36 @@
+const path = require("path")
+const webpack = require("webpack");
+
+module.exports = {
+  entry: {
+    bundle: "./javascript/application.js"
+  },
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve("./", "public")
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ]
+  },
+}
