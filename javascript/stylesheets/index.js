6debb96b80f8abe18ecf990d50082556f49046e0
Jiří Čech <jiricech94@gmail.com>
2021-01-27 11:10:40 +0100

    Restyle form

--- javascript/stylesheets/index.js
+++ javascript/stylesheets/index.js
@@ -1,3 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "select2/dist/css/select2.css";
 import "@ttskch/select2-bootstrap4-theme/dist/select2-bootstrap4.min.css";
+
+import "./application.css";
